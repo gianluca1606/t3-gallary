6bd319d23b0e75ff67e25a18aac1d1af6648e864
Gianluca Santos Latina <gianlucalatina@gmail.com>
2024-04-21 15:55:37 +0200

    add image upload

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,12 +18,13 @@ import {
  */
 export const createTable = pgTableCreator((name) => `t3-gallary_${name}`);
 
-export const posts = createTable(
+export const images = createTable(
   "image",
   {
     id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }).notNull(),
     url: varchar("url", { length: 256 }).notNull(),
+    userId: varchar("user_id", { length: 256 }).notNull(),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
