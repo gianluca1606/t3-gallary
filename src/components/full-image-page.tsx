d0e64885d45ad74e903f2a7463c353b2861fad7a
Gianluca Santos Latina <gianlucalatina@gmail.com>
2024-04-26 22:33:59 +0200

    delete button added

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,5 +1,6 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { getImage } from "~/server/queries";
+import { deleteImage, getImage } from "~/server/queries";
+import { Button } from "./ui/button";
 
 export async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
@@ -22,6 +23,19 @@ export async function FullPageImageView(props: { id: number }) {
         <div className="p-2">
           <div>Created On:</div>
           <div>{image.createdAt.toLocaleDateString()}</div>
+        </div>
+
+        <div className="p-2">
+          <form
+            action={async () => {
+              "use server";
+              await deleteImage(image.id);
+            }}
+          >
+            <Button type="submit" variant="destructive">
+              Delete
+            </Button>
+          </form>
         </div>
       </div>
     </div>
